e2c6d01db8656e0f7e272abe15a17a0cf5ce7095
Charliesolo <charliesolomon99@googlemail.com>
2024-12-20 10:58:49 +0000

    experimanet with sys.argv

--- libraries/name.py
+++ libraries/name.py
@@ -1,4 +1,11 @@
 import sys
 
+# if len(sys.argv) < 2:
+#     sys.exit
+# elif len(sys.argv) > 2:
+#     sys.exit
+# else:
+#     print("hello my name is", sys.argv[1] )
 
-print("hello my name is", sys.argv[1] )
+for arg in sys.argv[1:]:
+    print( "hello my name is ", arg)
