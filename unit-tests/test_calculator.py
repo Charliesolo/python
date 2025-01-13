4187d5513dc50ac89311f5dd7967a155c47a724c
Charliesolo <charliesolomon99@googlemail.com>
2025-01-13 10:01:43 +0000

    imporved basic tests

--- unit-tests/test_calculator.py
+++ unit-tests/test_calculator.py
@@ -4,12 +4,18 @@ def main():
     test_square()
 
 def test_square():
-    if square(2) != 4:
+    try:
+        assert square(2) == 4
+    except AssertionError:
         print("2 squared was not 4")
-    if square(3) != 9:
+    try:
+        assert square(3) == 9
+    except AssertionError:
         print("3 squared was not 9")
-    if square(4) != 16:
-        print("3 squared was not 9")
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("-3 squared was not 9")
 
 if __name__ == "__main__":
     main()
